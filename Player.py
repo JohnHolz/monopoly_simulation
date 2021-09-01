d67f35f6818002da1c3cfce58a8f6168eb492675
João Holz <joaopaulo.paivaholz@gmail.com>
2021-08-31 23:52:24 -0300

    finished tabuleiro.py functions

--- Player.py
+++ Player.py
@@ -1,13 +1,20 @@
 from numpy.random import randint
 
 class Player():
-    def __init__(self, money=31, position = 1):
+    def __init__(self, player_name, money=31, position = 1):
         self.position = position
         self.money = money
-            
+        self.play_next_round = True
+        self.player_name = player_name
+
     def __repr__(self):
-        return f"money:{self.money}||position:{self.position}"
-    
+        ret =  f"""
+        name:{self.player_name}
+        money:{self.money}
+        position:{self.position}
+        next round:{self.play_next_round}"""
+        return ret
+
     def __len__(self):
         return self.position
     
@@ -20,4 +27,5 @@ class Player():
     def roll_dice(self, die = 6):
         self.position = (self.position+randint(die)+1)%33
         
-        
+    def not_play_next_round(self):
+        self.play_next_round = False
--- tabuleiro.py
+++ tabuleiro.py
@@ -7,10 +7,13 @@ def card(player):
     else:
         player.update_money(-2)
 
-def func_1(player):
-    player.update_money(2)
-        
-def stop_propriedade(player_name,number):
+def update_money(value):
+    def output_function(player):
+        player.update_money(value)
+    return output_function
+
+def stop_propriedade(player):
+    number = player.position
     # valores da propriedadde
     owner = propriedades[number]['owner']
     price = propriedades[number]['price']
@@ -18,46 +21,53 @@ def stop_propriedade(player_name,number):
     
     if  owner == None:
         ## 1. checar se já tem dono
-        eval(player_name).update_money(-price)
-        propriedades[number]['owner'] = player_name
+        player.update_money(-price)
+        propriedades[number]['owner'] = player.player_name
         return
-    elif owner == player_name:
+    elif owner == player.player_name:
         return
     else:
         eval(owner).update_money(+pay)
-        eval(player_name).update_money(-pay)
-        return        
+        player.update_money(-pay)
+        return
+
+def next_round_without_play(player):
+    player.play_next_round = False
+
+def play_round(player):
+    player.roll_dice
+    tabuleiro[str(player.position)](player)
 
 tabuleiro = dict()
-tabuleiro['1'] = func_1
+tabuleiro['1'] = update_money(+2)
 tabuleiro['2'] = card
 tabuleiro['3'] = stop_propriedade
 tabuleiro['4'] = stop_propriedade
 tabuleiro['5'] = card
-#tabuleiro['6'] = 
+tabuleiro['6'] = play_round
 tabuleiro['7'] = stop_propriedade
 tabuleiro['8'] = stop_propriedade
-#tabuleiro['9'] = 
+tabuleiro['9'] = update_money(-2)
 tabuleiro['10'] = card
-#tabuleiro['11'] = 
+tabuleiro['11'] = next_round_without_play
 tabuleiro['12'] = stop_propriedade
 tabuleiro['13'] = stop_propriedade
-#tabuleiro['14'] = 
+tabuleiro['14'] = play_round
 tabuleiro['15'] = stop_propriedade
 tabuleiro['16'] = stop_propriedade
-#tabuleiro['17'] = 
+tabuleiro['17'] = update_money(+3)
 tabuleiro['18'] = card
 tabuleiro['19'] = stop_propriedade
 tabuleiro['20'] = stop_propriedade
 tabuleiro['21'] = card
-#tabuleiro['22'] = 
+tabuleiro['22'] = play_round
 tabuleiro['23'] = stop_propriedade
 tabuleiro['24'] = stop_propriedade
-#tabuleiro['25'] = 
+tabuleiro['25'] = update_money(-2)
 tabuleiro['26'] = card
-#tabuleiro['27'] = 
+tabuleiro['27'] = next_round_without_play
 tabuleiro['28'] = stop_propriedade
 tabuleiro['29'] = stop_propriedade
-#tabuleiro['30'] = 
+tabuleiro['30'] = play_round
 tabuleiro['31'] = stop_propriedade
 tabuleiro['32'] = stop_propriedade
